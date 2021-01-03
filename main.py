e02fbf05c3d0f202200b939a2ef09a4a2ee0294e
andrew <you@exampel.com>
2021-01-03 09:07:27 +0300

    first laucnh

---
+++ main.py
@@ -0,0 +1,37 @@
+import turtle
+
+
+def main():
+    turtle.shape('turtle')
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    for i in range(100):
+        print(i)
+        print(i**2)
+
+
+
+
+print('her')
+print('her')
+print('her')
+print('her')
+
+print('her')
+
+
+print('him')
+print('him')
+print('him')
+print('him')
+print('him')
+print('him')
+print('him')
+main()
